2b0912791e05437ff0c7a075a1d1fc5a6f7c9a55
Triplerush <fcanal@unsa.edu.pe>
2022-05-05 10:02:20 -0500

    Añadiendo la funcion 'div' a la clase Calculator

--- Calculator.java
+++ Calculator.java
@@ -2,7 +2,14 @@ class Calculator {
   int add(int a, int b){ return 0; }
   int sub(int a, int b){ return 0; }
   int mul(int a, int b){ return 0; }
-  int div(int a, int b){ return 0; }
-  int mod(int a, int b){ return 0; }
+
+  int div(int a, int b){
+		if (b == 0){
+			return 0;
+	  }
+		int result = a/b;
+		return result;
+	}
+	
 }
 
